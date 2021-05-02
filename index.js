042481e96c33ff25c065ae9e1a93dd490f690093
RodrigoJambeiro <rodrigo.portela@ucsal.edu.br>
2021-05-02 12:02:43 -0300

    3 files modified

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Import stylesheets
 import './style.css';
+import './css/bootstrap.css';
+import './js/bootstrap.js';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//const appDiv = document.getElementById('app');
+//appDiv.innerHTML = `<h1>JS Starter</h1>`;
